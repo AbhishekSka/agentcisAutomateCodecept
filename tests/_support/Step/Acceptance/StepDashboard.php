1ebe247d43967e69735b17d1ddd56f4db07ed91c
Abinash <abinash.shrestha@introcept.co>
2017-12-19 16:48:47 +0545

    2017.12.19 4:48

--- tests/_support/Step/Acceptance/StepDashboard.php
+++ tests/_support/Step/Acceptance/StepDashboard.php
@@ -6,6 +6,21 @@ class StepDashboard extends \AcceptanceTester
     public function Linktest()
     {
 
-    }
+        $user->loginAsAdmin();
+        $modules = [
+            '/clients' => 200,
+            '/partners' => 404,
+            '/prospects',
+            '/teams' => 302,
+            '/profile',
+            'invoice',
+            '/service/1/general/workflow/1'
+        ];
 
+        foreach ($modules as $module => $status) {
+            $I->amOnPage($module);
+            $I->seeResponseCodeIs($status);
+        }
+    }
 }
+
